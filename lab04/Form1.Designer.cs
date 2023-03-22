af901806e2048d240c29372fa582fc800560e9ed
oleksandrsidletskyi <sidleckiy.av@gmail.com>
2023-03-22 15:07:06 +0200

    Added new project to solution explorer called 'lab05'

--- lab04/Form1.Designer.cs
+++ lab04/Form1.Designer.cs
@@ -158,7 +158,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Lab04";
             this.ResumeLayout(false);
             this.PerformLayout();
 
