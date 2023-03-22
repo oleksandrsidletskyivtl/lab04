1cb1dc0b722ca0b5bb6c04d1ccc1f38901ee68b1
oleksandrsidletskyi <sidleckiy.av@gmail.com>
2023-03-22 14:59:34 +0200

    Added Design and coded functionality

--- lab04/Form1.cs
+++ lab04/Form1.cs
@@ -1,10 +1,35 @@
-namespace lab04
+﻿namespace lab04
 {
     public partial class Form1 : Form
     {
+        TextBox[] textBoxes;
+
         public Form1()
         {
             InitializeComponent();
+
+            textBoxes = new TextBox[5];
+
+            textBoxes[0] = textBox1;
+            textBoxes[1] = textBox2;
+            textBoxes[2] = textBox3;
+            textBoxes[3] = textBox4;
+            textBoxes[4] = textBox5;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.ResetText();
+
+            for (int idx = 0; idx < textBoxes.Length; idx++)
+            {
+                int num = idx + 1;
+
+                if (string.IsNullOrEmpty(textBoxes[idx].Text))
+                    richTextBox1.AppendText("Елемент " + num + ": Порожньо\n");
+                else
+                    richTextBox1.AppendText("Елемент " + num + ": " + textBoxes[idx].Text + '\n');
+            }
         }
     }
 }
